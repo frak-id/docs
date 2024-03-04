343f6dc6185494424c16e3bd745a368ee36f4d8b
KONFeature <quentin@nivelais.com>
2024-03-04 16:15:57 +0100

    🐛 Fix site deployment via pnpm and sst 2.0

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-This is a the repository for [Frak's documentation](https://docs.frak.id), created with [Vocs](https://vocs.dev) and using [sst-ion](https://github.com/sst/ion) for deployment.
-
-### Deployment
-
-Need bun, project installed, and sst-ion setup on your machine.
-
-To deploy, run:
-```bash
-sst --stage prod deploy
-```
+This is a the repository for [Frak's documentation](https://docs.frak.id), created with [Vocs](https://vocs.dev).
--- sst.config.ts
+++ sst.config.ts
@@ -1,31 +1,56 @@
-/// <reference path="./.sst/platform/src/global.d.ts" />
+import type { SSTConfig } from "sst";
+import {StaticSite} from "sst/constructs";
 
-export default $config({
-  app({stage}) {
-    if (stage !== "prod") {
-      throw new Error("Invalid stage");
-    }
-
+export default {
+  config(_input) {
     return {
       name: "docs",
-      providers: {
-        aws: {
-          region: "eu-west-1",
-        },
-      },
-      removalPolicy: "remove",
+      region: "eu-west-1",
     };
   },
-  async run() {
-    new sst.aws.StaticSite("DocsSite", {
-      domain: {
-        domainName: "docs.frak.id",
-        hostedZone: "frak.id",
+  stacks(app) {
+    // Remove all resources when non-prod stages are removed
+    app.setDefaultRemovalPolicy("destroy");
+    // Global function properties
+    app.setDefaultFunctionProps({
+      // Log param's
+      logRetention: "three_days",
+      // Build params
+      nodejs: {
+        // Minify code
+        minify: true,
+        // Disable source map for non ci/cd stage
+        sourcemap: false,
+        // Build options for esbuild
+        esbuild: {
+          // Bundle the packages
+          bundle: true,
+          // Always enable tree shaking
+          treeShaking: true,
+          // Override log level
+          logLevel: "warning",
+        },
       },
-      build: {
-        command: "bun run build",
-        output: "src/dist",
-      },
+      // Runtime node env
+      runtime: "nodejs20.x",
+      // Use arm64
+      architecture: "arm_64",
+      // Disable xray tracing
+      tracing: "disabled",
     });
+
+    // Deploy the docs site
+    app.stack(({stack}) => {
+      new StaticSite(stack, "DocsSite", {
+        path: "web/",
+        customDomain: {
+          domainName: "docs.frak.id",
+          hostedZone: "frak.id",
+        },
+        buildCommand: "pnpm run build",
+        buildOutput: "src/dist",
+      });
+    })
   },
-});
+} satisfies SSTConfig;
+
