59692b00d393d790e65bdf64247dc694eccabc4c
KONFeature <quentin@nivelais.com>
2024-10-16 19:56:52 +0200

    🚧 Startup business doc

--- web/sidebars/business.ts
+++ web/sidebars/business.ts
@@ -1,4 +1,17 @@
 /**
  * Represents the sidebar for the business documentation.
  */
-export const businessSidebar = [];
+export const businessSidebar = [
+    {
+        text: "Introduction ad manager",
+        link: "/business"
+    },
+    {
+        text: "Getting started",
+        link: "/business/getting-started"
+    },
+    {
+        text: "Keywords",
+        link: "/business/keywords"
+    }
+];
